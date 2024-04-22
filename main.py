e8090a80647684afd16d4efcc753f99bcee83816
marcelovca90 <marcelovca90@gmail.com>
2024-04-22 15:22:22 -0300

    Run with npz input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import numpy as np
 from pprint import pprint
 
 from sklearn.metrics import r2_score
@@ -6,27 +6,27 @@ from sklearn.model_selection import train_test_split
 
 from autosklearn.regression import AutoSklearnRegressor
 
-df = pd.read_parquet('synthetic.parquet')
+data = np.load('fas_reduced_dataset.npz')
+X, y = data['X'], data['y']
 
-X, y = df.filter(like='x'), df.filter(like='y')
-
-print('\n\nX shape and columns', X.shape, X.columns.tolist())
-print('\n\ny shape and columns', y.shape, y.columns.tolist())
+print('\nX shape', X.shape)
+print('\ny shape', y.shape)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 automl = AutoSklearnRegressor(time_left_for_this_task=300,
                               per_run_time_limit=60,
                               memory_limit=8*1024,
+                              n_jobs=1,
                               seed=42)
 
 automl.fit(X_train, y_train, dataset_name='synthetic')
 
-print('\n\nLeaderboard\n\n')
+print('\nLeaderboard\n')
 print(automl.leaderboard())
 
-print('\n\nModels\n\n')
+print('\nModels\n')
 pprint(automl.show_models(), indent=4)
 
 predictions = automl.predict(X_test)
-print("'\n\nR2 score:", r2_score(y_test, predictions))
+print("'\nR2 score:", r2_score(y_test, predictions))
